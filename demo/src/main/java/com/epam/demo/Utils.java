157c6e9807bf73e9100ede9ca6851b69fbfd1c21
Maksym Norokha <maxnorohich@gmail.com>
2024-04-04 21:28:15 +0200

    123

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,20 +1,25 @@
 package com.epam.demo;
 
 import java.util.List;
-import static com.epam.utils.StringUtils.isPositiveNumber;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        boolean isPositive = false;
-        if(args != null) {
-            if(!args.isEmpty()) {
-                for (String arg : args) {
-                    isPositive = isPositiveNumber(arg);
-                    if(!isPositive) {
-                        return false;
-                    }
+        if (args.isEmpty() || args==null) {
+            return false;
+        }
+        for (String s : args) {
+            if (s.isEmpty() || s==null) {
+                return false;
+            }
+            try {
+                if (Double.parseDouble(s)<=0) {
+                    return false;
                 }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return false;
             }
         }
-        return isPositive;
+        return true;
     }
 }
