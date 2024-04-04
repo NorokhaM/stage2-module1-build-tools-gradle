20003e35fb3813de0cf965ff7e7f09cf98a19436
Maksym Norokha <maxnorohich@gmail.com>
2024-04-04 21:18:28 +0200

    123

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         return str.matches("\\d+");
+         return str.matches("\\d+") && str!=null && !str.isEmpty();
     }
 }
